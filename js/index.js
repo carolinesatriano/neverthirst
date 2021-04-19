f24c0a6ef2a1570316d03cf46abe9883ba29d17a
carolinesatriano <carolinesatriano@gmail.com>
2021-04-19 18:32:46 -0500

    Almost Done

--- js/index.js
+++ js/index.js
@@ -45,10 +45,11 @@ jQuery(document).ready(function($) {
     $("span.counter").counterUp({
         delay: 40, /* The delay in milliseconds per number count up */
         time: 1000, /*The total duration of the count up animation */
-        offset: 100, 
+        offset: 80, 
         /*The viewport percentile from which the counter starts (by default it's 100, meaning it's triggered at the very moment the element enters the viewport) */
     });
   
+    
   });
 
 //This is for the water drop scroll progress
