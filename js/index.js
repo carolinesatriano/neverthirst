05e09944959f3b2a5ccc3d654fde7757c74956cd
carolinesatriano <carolinesatriano@gmail.com>
2021-04-20 10:57:15 -0500

    Final

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 /* Caroline Start */
+var lastScrollTop = 0;
+
 //This is to get the appearing text
 jQuery(document).ready(function($) {
   
@@ -14,7 +16,7 @@ jQuery(document).ready(function($) {
          var $animatable = $(this);
         
         // Items that are "above the fold"
-              if (($animatable.offset().top + $animatable.height() + 50) < offset) {
+              if (($animatable.offset().top + $animatable.height() + 60) < offset) {
           
           // Item previously wasn't marked as "above the fold": mark it now
           if (!$animatable.hasClass('animate-in')) {
@@ -24,7 +26,7 @@ jQuery(document).ready(function($) {
               }
         
         // Items that are "below the fold"
-        else if (($animatable.offset().top + $animatable.height() + 50) > offset) {
+        else if (($animatable.offset().top + $animatable.height() + 40) > offset) {
           
           // Item previously wasn't marked as "below the fold": mark it now
           if ($animatable.hasClass('animate-in')) {
@@ -49,7 +51,6 @@ jQuery(document).ready(function($) {
         /*The viewport percentile from which the counter starts (by default it's 100, meaning it's triggered at the very moment the element enters the viewport) */
     });
   
-    
   });
 
 //This is for the water drop scroll progress
@@ -141,123 +142,6 @@ $window.on('scroll', check_if_in_view);
 $window.on('load', check_if_in_view);
 
 
-
-//donations
-var amount;
-var reach;
-
-var x = $(window).width() - 400;
-
-$('.donate form').on("click", function(){
-  amount = $('input[name=amount]:checked', '#donAmount').val();
-  reach = amount * 22;
-  $('#confirm .amount').text("$" + amount);
-  $('#check span').text("$" + amount);
-  $('#confirm strong').text(reach + " voters");
-});
-
-$(".donate button").on("click", function(){
-  $(".donate").toggleClass("active");
-  if( $(".donate").is(".active") ) {
-    $("form").slideDown(450, "easeOutQuart");
-  } else {
-    $("form").slideUp(300, "easeInOutQuad");
-  }
-});
-
-$('.donate label').on("click", function(){
-  setTimeout(function() {
-    if (amount == "other"){
-      $("#custom").css("margin-left", x/2);
-$("#custom").css("margin-right", x/2);
-      $('body').addClass('custom');
-    	$(".donate").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    		$("#custom").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
-    	});
-  	} else {
-      $('body').removeClass('custom');
-    	$(".donate").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    		$("#details").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
-  		});
-  	}
-  }, 150);
-});
-
-$('#custom .next').on("click", function(){
-  amount = $('input[name=custom-amount]', '#customDonation').val();
-  reach = amount * 22;
-  $('#confirm .amount').text("$" + amount);
-  $('#check span').text("$" + amount);
-  $('#confirm strong').text(reach + " voters");
-  $("#custom").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    $("#details").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
-  });
-});
-
-$('#custom .back').on("click", function(){
-  $("#custom").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
-    $(".donate").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
-  });
-});
-
-$('#details .next').on("click", function(){
-  console.log(amount);
-  $("#details").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    $("#card").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
-  });
-});
-
-$('#details .back').on("click", function(){
-  
-  if ( $('body.custom').length > 0 ){
-      $("#details").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
-    $("#custom").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
-  });
-  } else {
-      $("#details").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
-    $(".donate").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
-  });
-  }
-  
-});
-
-$('#card .next').on("click", function(){
-  $("#card").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-  });
-});
-
-$('#card .back').on("click", function(){
-  $("#card").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
-    $(".details").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
-	});
-});
-
-$('#card .next').on("click", function(){
-  $("#card").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    $("#check").show("slide", { easing: "easeOutQuart", direction: "right" }, 700);
-  });
-});
-
-$('#check .back').on("click", function(){
-  $("#check").hide("slide", { easing: "easeInQuart", direction: "right" }, 700, function(){
-    $("#card").show("slide", { easing: "easeOutQuart", direction: "left" }, 700);
-	});
-});
-
-$("#check .next").on('click', function(){
-  $("#check").hide("slide", { easing: "easeInQuart", direction: "left" }, 700, function(){
-    $(".processing").fadeIn(1500, function(){
-      $(".progress").animate({ width: "14em" }, 3500, "easeInOutCirc", function() {
-        $( ".mask" ).hide("slide", { easing: "easeInQuart", direction: "right" }, 400);
-      });
-    });
-  });
-  
-	setTimeout(function() {
-		$('.processing .message, .outer').fadeOut();
-  	$("#confirm").addClass('animated fadeInUp');
-	}, 6250);
-});
 
 /* Caroline End */
 
